c4826f9b248044016327e7422f25f80bce4c32c6
arybjan <arybjen92@gmail.com>
2022-11-05 14:42:45 +0600

    added fields for model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Job(models.Model):
+    company_name = models.ForeignKey("jobs.CompanyName", on_delete=models.CASCADE)
+    vacancy_name = models.CharField(_("Наименование вакансии"), max_length=150)
+    price = models.FloatField(_("Заработная плата"))
+    description = models.TextField(_("Описание"))
+    created_at = models.DateTimeField(_("Дата публикации"), auto_now=True)
+
+
+class CompanyName(models.Model):
+    name = models.CharField(_("Наименование компании"), max_length=200)
