a95b4e58d5ff30e9ab0616d1b651b89ca6ba6c1e
arybjan <arybjen92@gmail.com>
2022-11-05 14:54:30 +0600

    wrote the views in jobs app

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,13 @@
 from rest_framework import viewsets
 from jobs.models import Job, CompanyName
+from jobs.serializer import JobsSerializer, CompanyNameSerializer
 
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
+    serializer_class = JobsSerializer
+
+
+class CompanyNameViewSet(viewsets.ModelViewSet):
+    queryset = CompanyName.objects.all()
+    serializer_class = CompanyNameSerializer
