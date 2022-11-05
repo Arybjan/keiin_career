b9e4881c3e871be1efffbb6efca4c9aa28ece647
arybjan <arybjen92@gmail.com>
2022-11-05 14:46:14 +0600

    admin log

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,27 @@
+from django.contrib import admin
+from .models import User, Genders
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "username",
+        "email",
+        "birthday_date",
+        "genders",
+        "created_at",
+    ]
+
+
+admin.site.register(User, UserAdmin)
+
+
+class GendersAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title"
+    ]
+
+
+admin.site.register(Genders, GendersAdmin)
